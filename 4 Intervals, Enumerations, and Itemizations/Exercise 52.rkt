7342de0567c8b3ac69c848c9262b25e5512c4b63
chen-qingyu <236521827@qq.com>
2021-07-22 09:33:24 +0800

    From GitHub to Gitee

---
+++ 4 Intervals, Enumerations, and Itemizations/Exercise 52.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 52|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+
+Which integers are contained in the four intervals above?
+
+[3, 5]: 3, 4, 5;
+(3, 5]: 4, 5;
+[3, 5): 3, 4;
+(3, 5): 4;
+
+|#
